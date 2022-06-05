7aa0f9713d9565f6493654de23a6931245502db9
Yonatan Martsiano <Yonatan888@gmail.com>
2022-06-05 13:08:22 +0300

    updates

--- contracts/ShebaStudy.sol
+++ contracts/ShebaStudy.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SHEBA {
-    enum Status{OpenForReview, UnderReview, ReviewPublished,Published }
-
-    string public name = "SHEBA";
+contract SHEBASTUDY {
+    // status : 0 open for review,1 under review,2 approved,3 need revision
+    string public name = "SHEBASTUDY";
     uint256 public fileCount = 1;
     uint public userIds = 1;
     address public admin;
     mapping(uint256 => File) public files;
     mapping(uint256 => address) public userById;
     mapping(address => Identity) public identities;
-    mapping (uint256=>Status) fileStatus;
     //each file number has several reviewers assigned
-    mapping (uint256=>address) ReviewerAssigned;
-    mapping (uint256=>address) PublisherChosen;
     mapping(string=>bool) named;
     struct File {
         uint256 fileId;
@@ -26,6 +22,9 @@ contract SHEBA {
         string paperType;
         uint256 connectedTo;
         address payable uploader;
+        uint256 status;
+        address publisher;
+        uint256 updateNumber;
     }
 
    struct Identity {
@@ -35,6 +34,8 @@ contract SHEBA {
         bool publisher;
         bool author;
         bool admin;
+        //the publisher employing this person if he is a reviewer 
+        address employer;
     }
 
     constructor(){
@@ -67,20 +68,38 @@ contract SHEBA {
 bool _manuscript=keccak256(abi.encodePacked(_paperType)) == keccak256(abi.encodePacked("Manuscript"));
 bool _review=keccak256(abi.encodePacked(_paperType)) == keccak256(abi.encodePacked("Review"));
 bool _update=keccak256(abi.encodePacked(_paperType)) == keccak256(abi.encodePacked("Update"));
-require(_manuscript||_review||_update,"File has to be of correct paper type");
+bool _needrevision=keccak256(abi.encodePacked(_paperType)) == keccak256(abi.encodePacked("NeedRevision"));
+require(_manuscript||_review||_update||_needrevision,"File has to be of correct paper type");
+uint _status;
+address _publisher;
+uint _updatedNumber=0;
+
       if (_manuscript) {
   require(identities[msg.sender].author,"only authors can upload manuscripts");
-  fileStatus[fileCount]=Status.OpenForReview;
-  PublisherChosen[fileCount]=address(0);
+  _status=0;
+  _publisher=address(0);
 }
 if (_review) {
   require(identities[msg.sender].reviewer,"only reviewers can upload reviews");
-  require(ReviewerAssigned[_connectedTo]==msg.sender,"Reviewer is not assigned for this file");
-    fileStatus[fileCount]=Status.ReviewPublished;
+  require(files[_connectedTo].publisher==identities[msg.sender].employer,"Reviewer is not working for this publisher");
+    _status=1;
+    _publisher=address(0);
 
 }
-if (_update) {
+if (_update) {  
   require(identities[msg.sender].author,"only authors can upload updates");
+  fileCount=_connectedTo;
+    _publisher=files[_connectedTo].publisher;
+    _updatedNumber=files[_connectedTo].updateNumber+1;
+    _subject=files[_connectedTo].subject;
+    _paperType=files[_connectedTo].paperType;
+    _connectedTo=files[_connectedTo].connectedTo;
+
+}
+ if (_needrevision) {
+require(identities[msg.sender].publisher,"only publishers upload revision requests");
+require(files[_connectedTo].publisher==msg.sender,"only relevent publisher can upload revision requests");
+  _status=3;
 }
 
 
@@ -93,7 +112,10 @@ if (_update) {
             _subject,
             _paperType,
             _connectedTo,
-            payable(msg.sender)
+            payable(msg.sender),
+            _status,
+            _publisher,
+            _updatedNumber
         );
         
                 fileCount++;
@@ -107,6 +129,7 @@ function newUser(address wallet,string memory _name) public {
         userIds++;
         identities[wallet].userName=_name;
         named[_name]=true;
+        identities[wallet].employer=address(0);
 }
 function getName(address _user) public view returns (string memory _name){
     require(identities[_user].userId!=0,"No such user in the system");
@@ -120,29 +143,38 @@ function isUser(address _user) public view returns (bool){
     }
 }
 
-    function assignReviewer(uint _file,address _reviewer) public{
-        require(identities[msg.sender].publisher,"only publishers can assign reviewers");
-        require(identities[_reviewer].reviewer,"can only assign reviewers");
-        require(PublisherChosen[_file]==address(0),"another publisher has already chosen this file");
-        ReviewerAssigned[_file]=_reviewer;
-        fileStatus[_file]=Status.UnderReview;
-        PublisherChosen[_file]=msg.sender;
-    }
-
         function returnFile(uint _file) public{
         require(identities[msg.sender].publisher,"only publishers can return files");
-        ReviewerAssigned[_file]=address(0);
-        fileStatus[_file]=Status.OpenForReview;
-        PublisherChosen[_file]=address(0);
+        require(files[_file].publisher==msg.sender,"only relevent publisher can return files");
+        files[fileCount].status=0;
+        files[_file].publisher=address(0);
     }
 
-     function confirmPublished(uint _file) public{
-        require(identities[msg.sender].author,"only authors can confirm files has been published");
+     function employReviewer(address _reviewer) public{
+        require(identities[msg.sender].publisher,"only publishers can return files");
+        require(identities[_reviewer].employer==address(0),"only unemployed reviewers can be employed");
+        identities[_reviewer].employer=msg.sender;
+    }
+
+      function reviewerUnmploy() public{
+        require(identities[msg.sender].reviewer,"only reviewers can unmploy themselves");
+       identities[msg.sender].employer=address(0);
+    }
+
+
+     function ApprovePublished(uint _file) public{
+        require(identities[msg.sender].author,"only authors can approve reviews");
         require(files[_file].uploader==msg.sender,"only file author can confirm a file has been published");
-        bool _manuscript=keccak256(abi.encodePacked(files[_file].paperType)) == keccak256(abi.encodePacked("Manuscript"));
-        require(_manuscript,"only a manuscript can be published");
-        fileStatus[_file]=Status.Published;
-        
+        bool _manuscript=keccak256(abi.encodePacked(files[_file].paperType)) == keccak256(abi.encodePacked("Manucsript"));
+        require(_manuscript,"only a manuscript can be approved");
+        files[fileCount].status=2;
+    }
+        function ApproveReview(uint _file) public{
+        require(identities[msg.sender].publisher,"only publishers can approve reviews");
+        require(identities[files[_file].uploader].employer==msg.sender,"only publisher employing reviewer can approve a review");
+        bool _review=keccak256(abi.encodePacked(files[_file].paperType)) == keccak256(abi.encodePacked("Review"));
+        require(_review,"only a review can be approved");
+        files[fileCount].status=2;
     }
   function setAuthor(address _user) public {
         require(identities[msg.sender].admin,"only admin can set author");
@@ -188,19 +220,13 @@ function isReviewer(address _user) public view returns(bool) {
         return identities[_user].reviewer;
     }
 
-    function getStatus(uint256 Id) public view returns(Status){
-          return fileStatus[Id];
+    function getStatus(uint256 Id) public view returns(uint){
+          return files[Id].status;
 
     }
     
-    function isReviewing(uint _file,address _reviewer) public view returns (bool){
-        if(ReviewerAssigned[_file]==_reviewer){
-            return true;
-        }
-        return false;
-    }
-        function isPublisherChosen(uint _file,address _publisher) public view returns (bool){
-        if(PublisherChosen[_file]==_publisher){
+        function isPublisherForFile(uint _file,address _publisher) public view returns (bool){
+        if(files[_file].publisher==_publisher){
             return true;
         }
         return false;
